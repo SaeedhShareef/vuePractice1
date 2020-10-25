fdbd7438afca39122d10a1fb3564d65e85e8e459
Saeed Shareef <saeedhshareef@gmail.com>
2020-10-25 00:31:05 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+ 
+  components: {
+    'app-header': Header,
+    'app-footer': Footer
+  },
+  data () {
+    return {
+
+    }
+   
+  }
+}
+</script>
+
+<style>
